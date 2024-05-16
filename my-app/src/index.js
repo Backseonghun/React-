ba8186906cbf2d950007fda8edcf0d980e96dad4
Backseonghun <tjdgns3690@gmail.com>
2024-05-16 10:41:11 +0900

    240516

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,6 +9,7 @@ import ConfirmDialog from './chapter04/ConfirmDialog';
 import Clock from './chapter04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+setInterval( () => {
 root.render(
   <React.StrictMode>
     {/* <App /> */}
@@ -18,6 +19,8 @@ root.render(
     <Clock/>
   </React.StrictMode>
 );
+},1000)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
